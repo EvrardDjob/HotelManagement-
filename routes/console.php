3b5e48d0ab0f99591368c0773c6bf0efbf434aff
EvrardDjob <evrarddjob41@gmail.com>
2026-01-23 21:29:30 +0100

    update notification features

--- routes/console.php
+++ routes/console.php
@@ -9,3 +9,4 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote');
 
 Schedule::command('app:check-expired-bookings')->daily();
+// Schedule::command('app:check-expired-bookings')->everyMinute();
